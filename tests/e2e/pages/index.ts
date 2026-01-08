adbef30b3707f4a077cf72ab5d9b2b611e49d9bf
Evan Hill <evan.hill@flashmath.io>
2026-01-08 03:13:38 +0000

    chore: ignore tests/ and test-results/ folders

--- tests/e2e/pages/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Page Object Model Exports
- */
-export { BasePage } from './base-page';
-export { SetupPage } from './setup-page';
-export { MatchPage } from './match-page';
-export { SocialPage } from './social-page';
-export { QueuePage } from './queue-page';
-
